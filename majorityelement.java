df9ff3eeb5cb4b226a73f33a05e61072de884159
Buena Peninnah Quadros <85785379+BuenaPeninnahQuadros@users.noreply.github.com>
2025-03-11 15:54:17 +0530

    Update majorityelement.java

--- majorityelement.java
+++ majorityelement.java
@@ -1,19 +1,7 @@
 class Solution {
-    public boolean searchMatrix(int[][] matrix, int target) {
-        int m = matrix.length;
-        int n = matrix[0].length;
-        int low = 0, high = m * n - 1;
-        while(low<=high){
-            int mid=(low+high)/2;
-            int mid_val=matrix[mid/n][mid%n];
-            if(mid_val==target){
-                return true;
-            }
-            else if(mid_val<target)
-            low=mid+1;
-            else
-            high=mid-1;
-        }
-        return false;
+    public int majorityElement(int[] nums) {
+        Arrays.sort(nums);
+        int n=nums.length;
+        return nums[n/2];
     }
 }
